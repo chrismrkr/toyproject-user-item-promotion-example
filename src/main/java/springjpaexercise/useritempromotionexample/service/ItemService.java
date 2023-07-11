938711abf1aa22d76d11eaf157411c067a0a7bb6
깃허브네임 <>
2023-07-12 07:05:22 +0900

    findItemByUserType method impl

--- src/main/java/springjpaexercise/useritempromotionexample/service/ItemService.java
+++ src/main/java/springjpaexercise/useritempromotionexample/service/ItemService.java
@@ -3,7 +3,10 @@ package springjpaexercise.useritempromotionexample.service;
 import springjpaexercise.useritempromotionexample.entity.Item;
 import springjpaexercise.useritempromotionexample.entity.dto.ItemDto;
 
+import java.util.List;
+
 public interface ItemService {
     Item create(ItemDto itemDto);
     Item update(ItemDto itemDto, Long id);
+    List<Item> findItemListByUserType(Long userId);
 }
--- src/main/java/springjpaexercise/useritempromotionexample/service/impl/ItemServiceImpl.java
+++ src/main/java/springjpaexercise/useritempromotionexample/service/impl/ItemServiceImpl.java
@@ -4,19 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springjpaexercise.useritempromotionexample.entity.Item;
+import springjpaexercise.useritempromotionexample.entity.User;
 import springjpaexercise.useritempromotionexample.entity.dto.ItemDto;
 import springjpaexercise.useritempromotionexample.entity.embeddable.StartEndDate;
 import springjpaexercise.useritempromotionexample.entity.enumtype.ItemType;
+import springjpaexercise.useritempromotionexample.entity.enumtype.UserStat;
+import springjpaexercise.useritempromotionexample.entity.enumtype.UserType;
 import springjpaexercise.useritempromotionexample.repository.ItemRepository;
+import springjpaexercise.useritempromotionexample.repository.UserRepository;
 import springjpaexercise.useritempromotionexample.service.ItemService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
     private final ItemRepository itemRepository;
+    private final UserRepository userRepository;
+
     private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
 
     @Override
@@ -56,6 +63,22 @@ public class ItemServiceImpl implements ItemService {
             findItem.updateEndDate(itemDto.getEndDate(), dateTimeFormatter);
         }
         return findItem;
+    }
+
+    @Override
+    public List<Item> findItemListByUserType(Long userId) {
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("USER NOT EXISTS"));
+        if(findUser.getUserStat().equals(UserStat.WITHDRAWAL)) {
+            throw new IllegalStateException("WITHDRAWAL USER CANNOT GET ITEM LIST");
+        }
+        if(findUser.getUserType().equals(UserType.ENTERPRISE)) {
+            return itemRepository.findByDateBetweenStartEndDate(LocalDate.now());
+        } else {
+            return itemRepository.findByItemTypeAndByDateBetweenStartEndDate(
+                    ItemType.GENERAL, LocalDate.now()
+                    );g
+        }
     }
 
 
